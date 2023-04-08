e8250756da0ab67ca29ccc0eb784eeb8aa033a5a
julio_filizzola <juliofilizzola@hotmail.com>
2023-04-08 15:34:34 -0300

    feat: create system service payment route

---
+++ src/main/java/com/filizzola/paymentservice/resource/impl/PaymentResourceImpl.java
@@ -0,0 +1,23 @@
+package com.filizzola.paymentservice.resource.impl;
+
+import com.filizzola.paymentservice.model.Payment;
+import com.filizzola.paymentservice.resource.PaymentResource;
+import com.filizzola.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping( value = "/payments")
+public class PaymentResourceImpl implements PaymentResource {
+
+  private final PaymentService paymentService;
+  @Override
+  public ResponseEntity<Payment> payment(Payment payment) {
+      paymentService.sendPayment(payment);
+      return ResponseEntity.status(HttpStatus.CREATED).build();
+  }
+}
